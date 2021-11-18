d5a270b9924ec53a2d9b871b19f2f3900843b64c
Guilhem <xxdarkwolf26xx@gmail.com>
2021-11-18 08:38:05 +0100

    first commit

---
+++ Halcoletmie/HalcoletmieApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalcoletmieApp.swift
+//  Halcoletmie
+//
+//  Created by Guilhem Schira on 18/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct HalcoletmieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
