1c256ca737935bad5f6f721776c02fe37e45267f
Guilhem <xxdarkwolf26xx@gmail.com>
2021-11-18 15:27:41 +0100

    Ajout d'une nouvelle page

---
+++ Halcoletmie/CocktailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CocktailViewController.swift
+//  Halcoletmie
+//
+//  Created by Guilhem Schira on 18/11/2021.
+//
+
+import UIKit
+
+class CocktailViewController: UIViewController {
+
+    var name: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
